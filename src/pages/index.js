f871175f16514105c7705279800bcb1d856f127c
Pedro Frohmut <pedrofrohmut@gmail.com>
2019-08-20 16:49:43 -0300

    Clean Up HomePage Links

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,6 @@ const IndexPage = function () {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>I'm PedroFrohmut, a full-stack developer living in Poá-SP</h2>
-      <p>
-        Need a developer?
-        {" "}
-        <Link to="/contact">Contact Me</Link>
-      </p>
     </Layout>
   )
 }
